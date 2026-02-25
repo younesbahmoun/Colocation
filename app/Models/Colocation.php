25d36366c3afb0aab291379abd3c021d9ea51b7a
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-25 11:03:43 +0000

    feat: add colocation Model

---
+++ app/Models/Colocation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Colocation extends Model
+{
+    protected $fillable = [
+        'statut',
+        'name',
+    ];
+}
