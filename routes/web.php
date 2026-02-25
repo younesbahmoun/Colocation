a16bdb6e3edac84761263315a29d794d87f316da
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-25 10:44:49 +0000

    feat: add routes Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ColocationController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('templates.index');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.show');
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register.show');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.store');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.store');
 });
